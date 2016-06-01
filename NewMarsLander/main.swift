a1a40954191c89f63bbb16ebf6c8fd461ce0b0ae
Svyatoshenko Misha <megal06@nightmail.ru>
2016-06-01 08:03:32 +0300

    Initial Commit

---
+++ NewMarsLander/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NewMarsLander
+//
+//  Created by Svyatoshenko "Megal" Misha on 2016/06/01.
+//  Copyright © 2016年 LoliTNC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
